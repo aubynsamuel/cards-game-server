45907b67eba99578645d8f0ebd9354f28c50b39a
Samuel Aubyn <aubynsamuel05@gmail.com>
2026-07-13 10:36:40 +0000

    refactor(firebase): improve Firebase app initialization and caching

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,32 @@
-import { applicationDefault, getApps, initializeApp } from "firebase-admin/app";
+import {
+  getApps,
+  initializeApp,
+  App,
+  cert,
+  ServiceAccount,
+} from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import serviceAccount from "../serviceAccountKey.json";
 
-const firebaseApp =
-  getApps()[0] ??
-  initializeApp({
-    credential: applicationDefault(),
-    projectId: process.env.FIREBASE_PROJECT_ID,
+let cachedApp: App | null = null;
+
+export function getFirebaseAdminApp() {
+  if (cachedApp) {
+    return cachedApp;
+  }
+
+  if (getApps().length > 0) {
+    cachedApp = getApps()[0]!;
+    return cachedApp;
+  }
+
+  cachedApp = initializeApp({
+    credential: cert(serviceAccount as ServiceAccount),
   });
 
-export const firebaseAuth = getAuth(firebaseApp);
-export const firestore = getFirestore(firebaseApp);
+  return cachedApp;
+}
+
+export const firebaseAuth = getAuth(getFirebaseAdminApp());
+export const firestore = getFirestore(getFirebaseAdminApp());
